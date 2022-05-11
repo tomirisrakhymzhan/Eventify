02c0513dfca7f156b612acd3e833c8bffe76f97b
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2022-05-11 02:56:26 +0100

    base onion structure, db and actvities entity adde

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        // private readonly ILogger<BaseApiController> _logger;
+
+        // public BaseApiController(ILogger<BaseApiController> logger)
+        // {
+        //     _logger = logger;
+        // }
+
+        // public IActionResult Index()
+        // {
+        //     return View();
+        // }
+
+        // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        // public IActionResult Error()
+        // {
+        //     return View("Error!");
+        // }
+    }
+}
