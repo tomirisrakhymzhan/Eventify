286a79c4be3b49ab201f745837db83a93451d723
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2022-05-13 02:50:07 +0100

    service container clean-up

--- API/Startup.cs
+++ API/Startup.cs
@@ -17,6 +17,8 @@ using MediatR;
 using Application.Activities;
 using AutoMapper;
 using Application.Core;
+using API.Extensions;
+
 
 namespace API
 {
@@ -35,23 +37,7 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt => 
-            {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt => 
-            {
-                opt.AddPolicy("CorsPolicy", policy => 
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
-            services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddApplicationServices(_config);
 
 
         }
