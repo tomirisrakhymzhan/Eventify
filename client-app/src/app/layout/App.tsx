e66ce69a3d6d7e1ebbd06c12d54769f4f4aac15f
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2022-05-13 03:27:34 +0100

    Merge branch 'main' of https://github.com/tomirisrakhymzhan/Eventify

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
 
 import Typography from '@mui/material/Typography';
@@ -12,7 +10,6 @@ function App() {
 
   useEffect(() => {
     axios.get('http://localhost:5000/api/activities').then(response => {
-      console.log(response);
       setActivities(response.data);
     })
   }, [])
