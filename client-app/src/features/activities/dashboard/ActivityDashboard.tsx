08b6dbf0fad7680e437ea24711860e7e8bb0a971
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2022-05-14 03:46:45 +0100

    axois api calls to actually make crud from client

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,18 +15,21 @@ interface Props{
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({activities, selectedActivity, 
     selectActivity, cancelSelectActivity, editMode, openForm, closeForm, 
-    createOrEdit, deleteActivity}:Props){
+    createOrEdit, deleteActivity, submitting}:Props){
     return(
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList 
                     activities={activities} 
                     selectActivity={selectActivity}
-                    deleteActivity={deleteActivity}/>
+                    deleteActivity={deleteActivity}
+                    submitting={submitting}/>
+
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity &&
@@ -39,7 +42,8 @@ export default function ActivityDashboard({activities, selectedActivity,
                 <ActivityForm
                     closeForm={closeForm} 
                     activity={selectedActivity} 
-                    createOrEdit={createOrEdit}/>}
+                    createOrEdit={createOrEdit}
+                    submitting={submitting}/>}
             </Grid.Column>
         </Grid>
     );
