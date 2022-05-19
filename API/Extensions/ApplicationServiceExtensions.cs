8fc2c73e73edb4c3afd06a0d30512c27fe4e099e
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2022-05-19 18:51:14 +0100

    cloudinary api image upload

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -9,6 +9,8 @@ using Microsoft.OpenApi.Models;
 using Persistence;
 using Application.Interfaces;
 using Infrastructure.Security;
+using Infrastructure.Photos;
+
 namespace API.Extensions
 {
     public static class ApplicationServiceExtensions
@@ -34,6 +36,9 @@ namespace API.Extensions
             services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
             services.AddScoped<IUserAccessor, UserAccessor>();
+            services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+            services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
+
             return services;
         }
     }
