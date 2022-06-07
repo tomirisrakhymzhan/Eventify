299d1591431f9430d67a672f5cfeb0139b1430f6
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2022-06-07 03:32:31 +0100

    comment date conversion bug fix

--- API/Startup.cs
+++ API/Startup.cs
@@ -62,7 +62,6 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
-
             // app.UseHttpsRedirection();
 
             app.UseRouting();
